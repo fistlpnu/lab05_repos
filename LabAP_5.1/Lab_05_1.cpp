678dec252021c53ed8f26c8270687eefb27c0899
Olexandr <fistovsky.laptop@gmail.com>
2024-10-15 19:36:17 +0300

    first commit

---
+++ LabAP_5.1/Lab_05_1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double g(const double a, const double b)
+{
+	return a * a + a * b + b * b;
+}
+
+int main()
+{
+	double s, t;
+
+	cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+
+
+	double g1_s = g(1, s); // 1 частина чисельника
+	double g_t1 = g(t, 1); // 2 частина чисельника
+	double chiselnik = g1_s + pow(1 + pow(g_t1, 2), 2); // Весь чисельник
+	double g_s_t1 = g(s + t, 1); // 1 частина знаменника
+	double znamennik = 1 + pow(g_s_t1, 3); // Весь знаменник
+
+	double result = chiselnik / znamennik;
+	
+	cout << "Result: " << result << endl;
+	
+
+	return 0;
+}
